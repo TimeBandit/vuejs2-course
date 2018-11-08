305990f005c1068bf63fcdedeb97416c0a27e7c5
timebandit <imran.nazir@yahoo.co.uk>
2018-11-08 21:36:39 +0000

    created folder for vue basics

---
+++ basics/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <p class="title">{{sayHello()}}</p>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "app",
+  data: function() {
+    return {
+      name: "Imran"
+    };
+  },
+  // components: {
+  //   HelloWorld
+  // },
+  methods: {
+    sayHello() {
+      return `Hello ${this.name}`;
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
