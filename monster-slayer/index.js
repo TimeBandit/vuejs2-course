36a6112b4327f881de29f7cc0a588985d9682945
timebandit <imran.nazir@yahoo.co.uk>
2018-11-14 10:29:42 +0000

    adding end of game logic
    TODO: implement watchers to end game when one player hits 0

--- monster-slayer/index.js
+++ monster-slayer/index.js
@@ -1,12 +1,13 @@
 console.log("running......");
+const log = console.log;
 
 const vm = new Vue({
 	el: "#app",
 	data: {
 		gameStarted: false,
-		youScore: 100,
+		playerScore: 100,
 		monsterScore: 100,
-		history: []
+		logs: []
 	},
 	methods: {
 		giveUp: function() {
@@ -16,27 +17,67 @@ const vm = new Vue({
 			// clear the list of actions
 			// reset the scores
 		},
-		attack: function() {
+		attack: function(e, playerHit=10, monsterHit=10) {
 			console.log("attacking");
 			let hit;
+			
 			// monster attacks you
-			hit = this.hitPower(10);
-			this.youScore -= hit;
-			this.history.push(`monster hits player for ${hit}`);
+			hit = this.hitPower(monsterHit);
+			const newPlayerScore = this.playerScore - hit;
+			newPlayerScore <= 0 ? this.playerScore = 0 : this.playerScore -= hit;
+			// this.playerScore -= hit;
+			this.logs.push({
+				isPlayer: true,
+				message:`monster hits player for ${hit}`
+			});
+			
 			// you attack monster
-			hit = this.hitPower(10);
-			this.monsterScore -= hit;
-			this.history.push(`player hits monster for ${hit}`);
+			hit = this.hitPower(playerHit);
+			const newMonsterScore = this.monsterScore - hit;
+			newMonsterScore <= 0 ? this.monsterScore = 0 : this.monsterScore -= hit;
+			// this.monsterScore -= hit;
+			this.logs.push({
+				isPlayer: false,
+				message:`player hits monster for ${hit}`
+			});
 		},
 		specialAttack: function() {
 			console.log("special attack");
+			this.attack(null, 20, 10);
 		},
-		heal: function() {
+		heal: function(e, playerHit=10, monsterHit=10) {
+			// TODO this is a special case of attack, can be refactored
 			console.log("healing");
+
+			const newPlayerScore = this.playerScore + 10;
+			// score cant be greater than 100;
+			if(newPlayerScore >= 100){
+				this.playerScore = 100;
+			} else {
+				// heal by 10
+				this.playerScore += 10;
+			}
+			this.logs.push({
+				isPlayer: true,
+				message:`player heals himself for 10`
+			});
+
+
+			// monster attacks you
+			hit = this.hitPower(monsterHit);
+			this.playerScore -= hit;
+			this.logs.push({
+				isPlayer: false,
+				message:`monster hits player for ${hit}`
+			});
 		},
 		hitPower: function(max = 100) {
-			return Math.ceil(Math.random(10) * 10);
+			return Math.ceil(Math.random() * max);
 		}
+	},
+	watch: {
+		playerScore: function(){},
+		monsterScore: function(){}
 	},
 	computed: {}
 });
