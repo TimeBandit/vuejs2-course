7f1ad58d227468c8beb6767c1b954805f05ed85f
timebandit <imran.nazir@yahoo.co.uk>
2018-11-26 10:31:52 +0000

    quotes project

---
+++ wonderful-quotes/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+    
+    </div>
+</template>
+
+<script>
+    export default {
+        data:function(){
+            return {
+                quotes:['Just a sample quote'],
+                maxQuotes: 10
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
