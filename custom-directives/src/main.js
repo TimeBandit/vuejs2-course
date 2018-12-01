ad27ad57fd9004aef02e5009c294e3c31b8ec567
Imran Nazir <imran.nazir@yahoo.co.uk>
2018-12-01 20:43:35 +0000

    custom directives

--- custom-directives/src/main.js
+++ custom-directives/src/main.js
@@ -6,7 +6,23 @@ import App from "./App.vue";
 // 4.componentUpdate 5.unbind 1&3 are most used
 Vue.directive("highlight", {
   bind(el, binding, vnode) {
-    el.style.backgroundColor = "green";
+    // arguments modifiers and values are three things
+    // you can work with when creating custom directives
+    // el.style.backgroundColor = "green";
+    // el.style.backgroundColor = binding.value;
+    // when using args
+    // when using a modifier
+    let delay = 0;
+    if (binding.modifiers["delayed"]) {
+      delay = 3000;
+    }
+    setTimeout(() => {
+      if (binding.arg === "background") {
+        el.style.backgroundColor = binding.value;
+      } else {
+        el.style.color = binding.value;
+      }
+    }, delay);
   }
 });
 
