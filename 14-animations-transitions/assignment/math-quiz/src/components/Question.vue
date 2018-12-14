a4f476057720d3525fe4d939b40d71448417c5ec
timebandit <imran.nazir@yahoo.co.uk>
2018-12-14 14:07:26 +0000

    renamed directories

--- 14-animations-transitions/assignment/math-quiz/src/components/Question.vue
+++ 14-animations-transitions/assignment/math-quiz/src/components/Question.vue
@@ -24,6 +24,7 @@ export default {
     };
   },
   methods: {
+    // TODO ensure that random data exclued the correct answer
     makeNewQuiz() {
       // change the quiz type
       this.type = this.type === "addition" ? "subtraction" : "addition";
