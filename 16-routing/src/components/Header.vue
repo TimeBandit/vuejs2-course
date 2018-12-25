4fb9490fd49aea83544ccec5bc1d6c8060496a69
Imran Nazir <imran.nazir@yahoo.co.uk>
2018-12-25 13:39:58 +0000

    vue routing

---
+++ 16-routing/src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <!-- <ul class="nav nav-pills">
+  <li class="nav-item">-->
+  <!-- no href attributes because it would -->
+  <!-- send the request to the server -->
+  <!-- <router-link to="/">Home</router-link>
+    </li>
+    <li class="nav-item">
+      <router-link to="/user">User</router-link>
+    </li>
+  </ul>-->
+  <ul class="nav nav-pills">
+    <router-link to="/" tag="li" active-class="active" exact>
+      <a>Home</a>
+    </router-link>
+    <router-link to="/user/1" tag="li" active-class="active">
+      <a>User 1</a>
+    </router-link>
+  </ul>
+</template>
+<script>
+export default {};
+</script>
---
+++ 16-routing/src/components/user/User.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>The User Page</h1>
+    <hr>
+    <p>Loaded ID: {{id}}</p>
+    <button @click="navigateToHome">Go to Home</button>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    id: {
+      type: String,
+      default: ""
+    }
+  },
+  data() {
+    return {};
+  },
+  // old way
+  // watch: {
+  // how to make sure new params on the same route/component
+  // combination get loaded
+  //   $route(to, from) {
+  //     this.id = to.params.id;
+  //   }
+  // },
+  methods: {
+    navigateToHome() {
+      this.$router.push("/");
+    }
+  },
+  created() {
+    console.log("🎄 ", this.id);
+  }
+};
+</script>
