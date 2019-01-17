a39a3f039eb12b95b27ae7c26525939e0fbfe9f4
timebandit <imran.nazir@yahoo.co.uk>
2019-01-17 09:03:06 +0000

    exercise 4

--- 02-interacting-with-dom/Exercise-4/app.js
+++ 02-interacting-with-dom/Exercise-4/app.js
@@ -10,7 +10,10 @@ const vm = new Vue({
   skewClass: "skewLeft",
   t3Input: "",
   t4aInput: "",
-  t4bInput: ""
+  t4bInput: "",
+  task5Input: "",
+  progress: 0,
+  progressStart: false
  },
  watch: {
   // *1 using a watch
@@ -29,6 +32,11 @@ const vm = new Vue({
   }
  },
  computed: {
+  styleObject: function() {
+   return {
+    width: this.progress + "%"
+   };
+  },
   t3Class: function() {
    if (this.t3Input === "red") {
     return "red";
@@ -38,18 +46,22 @@ const vm = new Vue({
     return "";
    }
   },
-   t4aClass: function () {
-     if (this.t4aInput === "red") {
-       return "red";
-     } else if (this.t4aInput === "yellow") {
-       return "yellow";
-     } else {
-       return "";
-     }
-   },
-   isOrange: function(){
-     if(this.){}else if(){}else{}
+  t4aClass: function() {
+   if (this.t4aInput === "red") {
+    return "red";
+   } else if (this.t4aInput === "yellow") {
+    return "yellow";
+   } else {
+    return "";
    }
+  },
+  isOrange: function() {
+   if (this.t4bInput === "true") {
+    return true;
+   } else if (this.t4bInput === "false") {
+    return false;
+   }
+  }
   // same as *1 but using a computed property
   // looks like a watch is the best way in this scenario
   // timer2: function() {
@@ -65,48 +77,13 @@ const vm = new Vue({
   // 		return null;
   // 	}
   // }
+ },
+ methods: {
+  startProgress(e) {
+   console.log("Progress started...", e);
+   this.progressStart;
+  }
  }
-	el: "#exercise",
-	data: {
-		start: false,
-		timer: null,
-		flip: false,
-		borderClass: "border",
-		animateClass: "changeColor"
-	},
-	watch: {
-		// *1 using a watch
-		start: function(val) {
-			console.log("start changed to ", val);
-			const self = this;
-			if (val) {
-				this.timer = setInterval(() => {
-					console.log("starting interval");
-					self.flip = !self.flip;
-				}, 1500);
-			} else {
-				console.log("stoping interval");
-				clearInterval(this.timer);
-			}
-		}
-	},
-	computed: {
-		// same as *1 but using a computed property
-		// looks like a watch is the best way in this scenario
-		// timer2: function() {
-		// 	const self = this;
-		// 	if (this.start) {
-		// 		return setInterval(() => {
-		// 			console.log("interval");
-		// 			self.flip = !self.flip;
-		// 		}, 1500);
-		// 	} else {
-		// 		console.log(this.timer2);
-		// 		if (this.timer2) clearInterval(this.timer2);
-		// 		return null;
-		// 	}
-		// }
-	}
 });
 
 console.log(vm);
