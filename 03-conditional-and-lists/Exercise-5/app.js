2a1a9088e3007efad19815293c1fefd78a5d01e7
timebandit <imran.nazir@yahoo.co.uk>
2019-01-24 09:57:21 +0000

    * Exercise 5

--- 03-conditional-and-lists/Exercise-5/app.js
+++ 03-conditional-and-lists/Exercise-5/app.js
@@ -1,16 +1,26 @@
+console.log("Go...");
 new Vue({
-  el: '#exercise',
-  data: {
-    array: ['Max', 'Anna', 'Chris', 'Manu'],
-    myObject: {
-      title: 'Lord of the Rings',
-      author: 'J.R.R. Tolkiens',
-      books: '3'
-    },
-    testData: {
-      name: 'TESTOBJECT', 
-      id: 10,
-      data: [1.67, 1.33, 0.98, 2.21]
-    }
+ el: "#exercise",
+ data: {
+  toggle: false,
+  myData: [],
+  array: ["Max", "Anna", "Chris", "Manu"],
+  myObject: {
+   title: "Lord of the Rings",
+   author: "J.R.R. Tolkiens",
+   books: "3"
+  },
+  testData: {
+   name: "TESTOBJECT",
+   id: 10,
+   data: [1.67, 1.33, 0.98, 2.21]
   }
+ },
+ mounted: function() {
+  const vm = this;
+  const chance = new Chance();
+  const arr = Array.from(Array(10));
+  const name = () => chance.name({ nationality: "en" });
+  arr.forEach(() => vm.myData.push(name()));
+ }
 });
