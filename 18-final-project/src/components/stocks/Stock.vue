86e14cc70d7f46f7b60563e57f6b8d780781cbb2
timebandit <imran.nazir@yahoo.co.uk>
2019-04-18 08:23:56 +0100

    * stocks component

--- 18-final-project/src/components/stocks/Stock.vue
+++ 18-final-project/src/components/stocks/Stock.vue
@@ -1,5 +1,20 @@
 <template>
-  <div>
-    stocks/stocks
+  <div class="col-sm-6 col-md-4">
+    <div class="panel panel-success">
+      <div class="panel-heading">
+        <h3 class="panel-title">
+          NAME
+          <small>(Price: PRICE)</small>
+        </h3>
+      </div>
+      <div class="panel-body">
+        <div class="pull-left">
+          <input type="number" class="form-control" placeholder="Quantity">
+        </div>
+        <div class="pull-right">
+          <button class="btn btn-success">Buy</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
